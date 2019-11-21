a9a6771e1573b23400144a798a68dbd69b526184
MEK <alessandro.rossi@cpnv.ch>
2019-11-21 21:26:49 +0100

    Added new library for the game and the login

---
+++ ARX_Remastered/MainMenuLib/Register.cs
@@ -0,0 +1,22 @@
+﻿/// 
+/// File name : Register.cs
+/// Author : Alessandro Rossi
+/// Date : 21.11.2019
+/// Description : This file contains code for register new user in a database.
+/// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMenuLib
+{
+    /// <summary>
+    /// This class contains method for registering new users in the database 
+    /// </summary>
+    public class Register
+    {
+
+    }
+}
