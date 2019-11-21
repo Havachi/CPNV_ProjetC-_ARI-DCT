cc3317a9ec554e8ad3f770e02e511959c2ec68ec
Alessandro Rossi <alessandro.rossi@cpnv.ch>
2019-11-21 14:57:41 +0100

    Added new project for DB connections and Db utilities

---
+++ ARX_Remastered/DBConnectionLib/DBConnection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace DBConnectionLib
+    {
+        public class DBConnection
+        {
+            private MySqlConnection connection;
+
+            public DBConnection()
+            {
+                InitConnection();
+            }
+
+            /// <summary>
+            /// Initialize the connection to the database
+            /// </summary>
+            private void InitConnection()
+            {
+                // Creation of the connection string : where, who
+                // Avoid user id and pwd hardcoded
+                string connectionString = "SERVER=127.0.0.1; DATABASE=arx_db; UID=DBConnector; PASSWORD=1234";
+                connection = new MySqlConnection(connectionString);
+            }
+
+            /// <summary>
+            /// Open connection to the database
+            /// </summary>
+            public void OpenConnection()
+            {
+                connection.Open();
+            }
+
+            /// <summary>
+            /// Close connection to the database
+            /// </summary>
+            public void CloseConnection()
+            {
+                connection.Dispose();
+            }
+
+        }
+    }
+
+}
