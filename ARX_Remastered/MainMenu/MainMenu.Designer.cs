9d9fd6f63e72b66cf731142838e8138783c14928
Alessandro Rossi <alessandro.rossi@cpnv.ch>
2019-11-21 14:08:48 +0100

    Je laisse damien faire le main menu

--- ARX_Remastered/MainMenu/MainMenu.Designer.cs
+++ ARX_Remastered/MainMenu/MainMenu.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.lblARXMainMenu = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(224, 162);
+            this.button1.Location = new System.Drawing.Point(141, 136);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,21 +42,33 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // lblARXMainMenu
+            // 
+            this.lblARXMainMenu.AutoSize = true;
+            this.lblARXMainMenu.Location = new System.Drawing.Point(156, 34);
+            this.lblARXMainMenu.Name = "lblARXMainMenu";
+            this.lblARXMainMenu.Size = new System.Drawing.Size(60, 13);
+            this.lblARXMainMenu.TabIndex = 1;
+            this.lblARXMainMenu.Text = "Main Menu";
+            // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(404, 381);
+            this.Controls.Add(this.lblARXMainMenu);
             this.Controls.Add(this.button1);
             this.Name = "MainMenu";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblARXMainMenu;
     }
 }
 
