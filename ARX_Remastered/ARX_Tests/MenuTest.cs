4f20b3c5c39a3feaac672578b960d9fc743660e6
Damien <damien.chervet@cpnv.ch>
2019-11-22 13:59:37 +0100

    Test for LoginForm created + add access RegiterInDB

---
+++ ARX_Remastered/ARX_Tests/MenuTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainMenuLib;
+using MainMenu;
+
+
+namespace ARX_Tests
+{
+    class MenuTest
+    {
+        [TestClass]
+        public class LoginFormTest
+        {
+            /// <summary>
+            /// Test if the login form
+            /// First, username and password are initialised
+            /// then the login is tested, with method from MainMenu\CheckData.cs
+            /// </summary>
+            [TestMethod]
+            public void TestLoginFormBothEmpty()
+            {
+                string username = "";
+                string password = "";
+                CheckData logincheck = new CheckData(username, password);
+
+
+                Assert.IsTrue(username == "");
+            }
+        }
+    }
+}
