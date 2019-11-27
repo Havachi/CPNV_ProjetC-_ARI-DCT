1da713d86ad54d6c09b6e7179fb1043a7959ddaa
Alessandro Privé <alessandro.rossi@cpnv.ch>
2019-11-27 22:36:02 +0100

    Commenting

--- ARX_Remastered/GameLib/Player.cs
+++ ARX_Remastered/GameLib/Player.cs
@@ -6,26 +6,44 @@ using System.Threading.Tasks;
 
 namespace GameLib
 {
+    /// <summary>
+    /// This Class contain method for creating and setup Player in the game
+    /// </summary>
     public class Player
     {
+        
         private string username;
         private double lifepoint;
         private Position position;
         private Inventory inventory;
         private string direction;
 
-        public Player(string userName, double lifePoint, Position position, Inventory inventory, string direction)
+        /// <summary>
+        /// Constructor for the player object
+        /// </summary>
+        /// <param name="username">This is the player username (from DB)</param>
+        /// <param name="lifePoint">This is the player lifeppoint (might not be used)</param>
+        /// <param name="position">This is the player current position (X and Y)</param>
+        /// <param name="inventory">This is the player inventory</param>
+        /// <param name="direction">This is the current player orientation (N,E,S,W)</param>
+        public Player(string username, double lifePoint, Position position, Inventory inventory, string direction)
         {
-            this.username = userName;
+            this.username = username;
             this.lifepoint = lifePoint;
             this.position = position;
             this.inventory = inventory;
             this.direction = direction;
         }
+        /// <summary>
+        /// Accessor for private attribut username
+        /// </summary>
         public string Username
         {
             get { return username; }
         }
+        /// <summary>
+        /// Accessor for private attribut lifepoint
+        /// </summary>
         public double Lifepoint
         {
             get
@@ -33,6 +51,9 @@ namespace GameLib
                 return lifepoint;
             }
         }
+        /// <summary>
+        /// Accessor for private attribut position
+        /// </summary>
         public Position Position
         {
             get
@@ -40,6 +61,9 @@ namespace GameLib
                 return position;
             }
         }
+        /// <summary>
+        /// Accessor for private attribut inventory
+        /// </summary>
         public Inventory Inventory
         {
             get
@@ -47,6 +71,9 @@ namespace GameLib
                 return inventory;
             }
         }
+        /// <summary>
+        /// Accessor for private attribut dirention
+        /// </summary>
         public string Direction
         {
             get { return direction; }
