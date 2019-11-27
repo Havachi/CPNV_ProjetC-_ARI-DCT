20bc542aaa2b4929b1ecf4312d8406462af683cb
Damien <damien.chervet@cpnv.ch>
2019-11-27 11:17:39 +0100

    Update Form

---
+++ ARX_Remastered/MainMenu/FormMainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainMenu
+{
+    public partial class FormMainMenu : Form
+    {
+        private string UserEmail;
+
+        public FormMainMenu()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (FormLogin formLogin = new FormLogin())
+            {
+               formLogin.ShowDialog(this);
+               lblMainMenuLogged.Text = $"Logged as {formLogin.Mail}";
+            }
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
