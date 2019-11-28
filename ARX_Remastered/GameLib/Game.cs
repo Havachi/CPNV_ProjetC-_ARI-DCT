52946475da419c37892deed1487eb7e8308b3aa3
Alessandro Rossi <alessandro.rossi@cpnv.ch>
2019-11-28 13:45:38 +0100

    added class for the game and cases

---
+++ ARX_Remastered/GameLib/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GameLib
+{
+    public class Game
+    {
+        private Player player;
+        private Map map;
+
+
+        public Game(Player player, Map map)
+        {
+            this.player = player;
+            this.map = map;
+        }
+
+        public Player Player
+        {
+            get { return player; }
+            set { player = value; }
+        }
+
+        public Map Map
+        {
+            get { return map; }
+            set { map = value; }
+        }
+
+    }
+}
