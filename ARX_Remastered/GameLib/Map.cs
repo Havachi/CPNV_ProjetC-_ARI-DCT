44cd52f8946a99abf7dee7e7ab61da8b1bd96c55
Alessandro Privé <alessandro.rossi@cpnv.ch>
2019-11-28 22:17:09 +0100

    In Progress: Updated map generation

--- ARX_Remastered/GameLib/Map.cs
+++ ARX_Remastered/GameLib/Map.cs
@@ -10,16 +10,18 @@ namespace GameLib
 {
     public class Map
     {
-        private List<Case> mapContent= new List<Case>();
+        private static int xMax = 10;
+        private static int yMax = 10;
+        private VoidCase voidCase = new VoidCase();
+        private Case[,] mapContent=new Case[xMax, yMax];
+        private List<List<bool>> borderMap = new List<List<bool>>();
         private int mapSeed;
         public Map()
         {
             mapSeed = GenerateSeed(20);
-            GenerateMap(mapSeed);
-        }
-        public Map(List<Case> mapContent)
-        {
-            this.mapContent = mapContent;
+           /// mapContent = GenerateVoidMap();
+            GenerateBorderMap();
+            //GenerateBorder
         }
 
         public int GenerateSeed(int seedSize)
@@ -34,12 +36,77 @@ namespace GameLib
             }
             
         }
-        public void GenerateMap(int levelSeed)
+        public void GenerateVoidMap()
         {
-
+            for (int i = 0; i < xMax; i++)
+            {
+                for (int j = 0; j < yMax; j++)
+                {
+                    mapContent.SetValue(voidCase,i,j);
+                }
+            }
         }
 
-        public List<Case> MapContent
+        public void GenerateBorderMap()
+        {
+            int iteration = new int();
+
+            bool isWallNorth = false;
+            bool isWallEast = false;
+            bool isWallSouth = false;
+            bool isWallWest = false;
+            bool collition;
+
+            Random rand = new Random();
+
+            
+
+            foreach (var mapCase in borderMap)
+            {
+                var caseType = rand.Next(0, 5);
+                iteration++;
+                switch (caseType)
+            {
+                //Void
+                case 0:
+
+                    break;
+                //Dead end
+                case 1:
+                    isWallNorth = true;
+                    isWallEast = true;
+                    isWallSouth = false;
+                    isWallWest = true;
+                    break;
+                //Corner
+                case 2:
+                    isWallNorth = true;
+                    isWallEast = false;
+                    isWallSouth = false;
+                    isWallWest = true;
+                    break;
+                //Corridor
+                case 3:
+                    isWallNorth = false;
+                    isWallEast = true;
+                    isWallSouth = false;
+                    isWallWest = true;
+                    break;
+                //T
+                case 4:
+                    isWallNorth = true;
+                    isWallEast = false;
+                    isWallSouth = false;
+                    isWallWest = false;
+                    break;
+            }
+                mapCase.Add(isWallNorth);
+                mapCase.Add(isWallEast);
+                mapCase.Add(isWallSouth);
+                mapCase.Add(isWallWest);
+            }
+        }
+        public Case[,] MapContent
         {
             get { return mapContent; }
         }
