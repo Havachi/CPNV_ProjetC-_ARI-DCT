08b6eca269a16a645b3ae9c6688aea09dea08148
Alessandro Rossi <alessandro.rossi@cpnv.ch>
2019-11-28 15:06:48 +0100

    Multiple changed in Map.cs

--- ARX_Remastered/GameLib/Map.cs
+++ ARX_Remastered/GameLib/Map.cs
@@ -5,48 +5,75 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Renci.SshNet;
 
 namespace GameLib
 {
     public class Map
     {
-        private List<Case> mapContent= new List<Case>();
-        private int mapSeed;
+        private static int columns =10;
+        private static int rows=10 ;
+        private int seedSize;
+
+        private int[,] mapContent = new int[columns,rows];
+        //private List<Case> mapContent = new List<Case>();
+        private VoidCase voidCase = new VoidCase();
+        private byte[] mapSeed;
+
+
         public Map()
         {
-            mapSeed = GenerateSeed(20);
-            GenerateMap(mapSeed);
+            seedSize = columns * rows;
+            mapSeed = GenerateSeed(seedSize);
+            LoadVoidMap();
         }
-        public Map(List<Case> mapContent)
+        public Map(int[,] mapContent)
         {
             this.mapContent = mapContent;
         }
 
-        public int GenerateSeed(int seedSize)
+        public byte[] GenerateSeed(int seedSize)
         {
             using (RNGCryptoServiceProvider rg = new RNGCryptoServiceProvider())
             {
 
-                byte[] rno = new byte[seedSize];
+                byte[] rno = new byte[10];
                 rg.GetBytes(rno);
-                int seed = BitConverter.ToInt32(rno, 0);
-                return seed;
+                //int seed = BitConverter.ToInt32(rno, 0);
+                return rno;
             }
             
         }
-        public void GenerateMap(int levelSeed)
+        public void LoadVoidMap()
         {
+            for (int x = 0; x < rows; x++)
+            {
+                for (int y = 0; y < columns; y++)
+                {
+                    mapContent[x,y] = 0;
 
+                }
+            }
         }
 
-        public List<Case> MapContent
+
+        public int[,] MapContent
         {
             get { return mapContent; }
         }
 
-        public int MapSeed
+        public byte[] MapSeed
         {
             get { return mapSeed; }
+        }
+
+        public int Rows
+        {
+            get { return rows; }
+        }
+        public int Columns
+        {
+            get { return columns; }
         }
     }
 
