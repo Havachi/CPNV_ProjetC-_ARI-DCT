e63d2e24ec90ff4d514e6f8190abe1972611afad
Alessandro Rossi <alessandro.rossi@cpnv.ch>
2019-11-29 12:11:37 +0100

    Code Cleanup and remove unused method

--- ARX_Remastered/DBConnectionLib/DBConnection.cs
+++ ARX_Remastered/DBConnectionLib/DBConnection.cs
@@ -62,98 +62,6 @@ namespace DBConnectionLib
         }
 
         /// <summary>
-        /// get the name of the player according to his id
-        /// </summary>
-        /// <param name="id">id of the player</param>
-        /// <returns></returns>
-        public string GetPlayerName(int id)
-        {
-            string name = "";
-            // Create a command object
-            OpenConnection();
-            MySqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = $"SELECT Username FROM users WHERE UserID ={id}";
-            
-            DbDataReader reader = cmd.ExecuteReader();
-            
-            if (reader.HasRows)
-            {
-                //we go through the result of the select, we might get only one response. 
-                //Despite this, we use a while
-                while (reader.Read())
-                {
-                    name = reader.GetString(0);
-                    //if a field can be null, we check if the result is not null before getting the value
-                    //if (!reader.IsDBNull(2))
-                    //{
-                    //    telContact = reader.GetString(2);
-                    //}
-                    CloseConnection();
-                }
-
-                return name;
-            }
-
-            return name;
-        }
-
-        /// <summary>
-        ///  Check if the username exist in database 
-        /// </summary>
-        /// <param name="username"></param>
-        /// <returns></returns>
-        public bool CheckUsername(string username)
-        {
-            OpenConnection();
-            MySqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = $"SELECT UserID FROM users WHERE Username = \"{username}\"";
-            DbDataReader reader = cmd.ExecuteReader();
-
-            if (reader.HasRows)
-            {
-                while (reader.Read())
-                {
-                    reader.Close();
-                    CloseConnection();
-                    return true;
-                }
-            }
-
-            throw new UnknownUsernameException();
-        }
-
-        /// <summary>
-        /// check if the password entered by the user is the same as the one in the database
-        /// </summary>
-        /// <param name="UserID"></param>
-        /// <param name="Password"></param>
-        /// <returns></returns>
-        public bool CheckPassword(string UserID, string Password)
-        {
-            OpenConnection();
-            string pswInDB;
-            MySqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = $"SELECT UserPassword FROM users WHERE UserID = \"{UserID}\"";
-            DbDataReader reader = cmd.ExecuteReader();
-
-            if (reader.HasRows)
-            {
-                while (reader.Read())
-                {
-                    pswInDB = reader.GetString(0);
-                    if (pswInDB == Password)
-                    {
-                        reader.Close();
-                        CloseConnection();
-                        return true;
-                    }
-                }
-            }
-
-            throw new InvalidPasswordException();
-        }
-
-        /// <summary>
         /// This method check if the userEmail exist in database
         /// </summary>
         /// <param name="userEmail"></param>
@@ -267,26 +175,6 @@ namespace DBConnectionLib
             }
 
             throw new UnknownUserEmailAddressException("This Email Address doesn\'t exist");
-        }
-
-        public string GetUserIDFromUserEmail(string UserEmail)
-        {
-            MySqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = $"SELECT UserID FROM users WHERE UserEmail =\"{UserEmail}\" ";
-            DbDataReader reader = cmd.ExecuteReader();
-            OpenConnection();
-            if (reader.HasRows)
-            {
-                while (reader.Read())
-                {
-                    var result = reader.GetString(0);
-                    reader.Close();
-                    CloseConnection();
-                    return result;
-                }
-            }
-
-            throw new UnknownUsernameException("This Username doesn\'t exist");
         }
 
         #endregion
