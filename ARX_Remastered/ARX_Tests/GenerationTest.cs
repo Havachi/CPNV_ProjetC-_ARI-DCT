d92952999245394cdd9c12471865fd5e9662421c
Alessandro Privé <alessandro.rossi@cpnv.ch>
2019-11-29 21:23:43 +0100

    Added tests for Generation

---
+++ ARX_Remastered/ARX_Tests/GenerationTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GameLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ARX_Tests
+{
+    [TestClass]
+    public class GenerationTest
+    {
+        [TestMethod]
+        public void TryGenerateTest()
+        {
+            var m = new Maze();
+            m.GenerateMaze();
+
+        }
+
+        [TestMethod]
+        public void TestIsAlreadyVisited()
+        {
+
+            var m = new Maze();
+            m.GenerateMaze();
+            var caseInfoList = m.CaseInfoList;
+            m.IsAlreadyVisited(caseInfoList);
+        }
+    }
+}
