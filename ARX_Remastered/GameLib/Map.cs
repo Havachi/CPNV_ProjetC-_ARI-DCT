5a095f96bf61b8b073ea1406542c747d917352d9
Alessandro Rossi <alessandro.rossi@cpnv.ch>
2019-12-05 14:49:07 +0100

    Changed Master's map.cs

--- ARX_Remastered/GameLib/Map.cs
+++ ARX_Remastered/GameLib/Map.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing.Text;
 using System.Linq;
@@ -11,40 +11,10 @@ namespace GameLib
 {
     public class Map
     {
-        private static int xMax = 10;
-        private static int yMax = 10;
-        private VoidCase voidCase = new VoidCase();
-        private Case[,] mapContent=new Case[xMax, yMax];
-        private List<List<bool>> borderMap = new List<List<bool>>();
-        private int mapSeed;
+
         public Map()
         {
 
         }
-
-        public void GenerateVoidMap()
-        {
-            for (int i = 0; i < xMax; i++)
-            {
-                for (int j = 0; j < yMax; j++)
-                {
-                    mapContent.SetValue(voidCase,i,j);
-                }
-            }
-        }
-
-
-
-
-
-
     }
-
-
-
-    
-    
-
-
-    
 }
