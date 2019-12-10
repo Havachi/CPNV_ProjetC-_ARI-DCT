da8037d7591b3bdb9b89b59cbe649ceeced0a5db
Alessandro Rossi <alessandro.rossi@cpnv.ch>
2019-12-10 12:12:17 +0100

    //WIP map graphics generation

---
+++ ARX_Remastered/ARX_Tests/MapTests/MapGraphicsTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameLib;
+namespace ARX_Tests
+{
+    [TestClass]
+    public class MapGraphicsTest
+    {
+        [TestMethod]
+        public void TryGenerateAnMapImage()
+        {
+            MapGraphics map = new MapGraphics();
+            map.DrawMap();
+        }
+    }
+}
--- ARX_Remastered/GameLib/Map Classes/MapGraphics.cs
+++ ARX_Remastered/GameLib/Map Classes/MapGraphics.cs
@@ -4,11 +4,25 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GameLib
 {
     public class MapGraphics
     {
-        
+
+        private Bitmap corner = new Bitmap(@"..\Assets\Map\Corner");
+
+
+        private static int maxHeight = 10;
+        private static int maxWidth = 10;
+        private int caseToDraw = maxHeight * maxWidth;
+
+        public void DrawMap()
+        {
+            Bitmap mapBitmap = new Bitmap(500, 500);
+            Graphics flagGraphics = Graphics.FromImage(mapBitmap);
+            mapBitmap.Save(@"C:\oscourmek.bmp");
+        }
     }
 }
