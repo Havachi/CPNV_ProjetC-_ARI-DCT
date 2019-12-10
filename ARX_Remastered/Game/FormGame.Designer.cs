9987a14614e680e4d6e4b33c449746fdfe508cde
Alessandro Rossi <alessandro.rossi@cpnv.ch>
2019-12-10 11:41:33 +0100

    Renamed project + Added Picture box for the map

--- ARX_Remastered/Game/FormGame.Designer.cs
+++ ARX_Remastered/Game/FormGame.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Game
 {
-    partial class Form1
+    partial class FormGame
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.picMap = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picMap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picMap
+            // 
+            this.picMap.Location = new System.Drawing.Point(466, 23);
+            this.picMap.Name = "picMap";
+            this.picMap.Size = new System.Drawing.Size(500, 500);
+            this.picMap.TabIndex = 0;
+            this.picMap.TabStop = false;
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(978, 548);
+            this.Controls.Add(this.picMap);
+            this.Name = "FormGame";
+            this.Text = "ARX Remastered";
+            ((System.ComponentModel.ISupportInitialize)(this.picMap)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picMap;
     }
 }
 
--- ARX_Remastered/Game/FormGame.cs
+++ ARX_Remastered/Game/FormGame.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Game
 {
-    public partial class Form1 : Form
+    public partial class FormGame : Form
     {
-        public Form1()
+        public FormGame()
         {
             InitializeComponent();
         }
