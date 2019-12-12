e22d4807d1cb50403f385ce6144ad8a08c52ba1a
Damien <damien.chervet@cpnv.ch>
2019-12-12 12:01:17 +0100

    Hotfixe - Sizeable form with button

--- ARX_Remastered/MainMenu/FormRegister.Designer.cs
+++ ARX_Remastered/MainMenu/FormRegister.Designer.cs
@@ -140,7 +140,7 @@
             this.Controls.Add(this.tbxRegisterMail);
             this.Controls.Add(this.lblRegisterTitle);
             this.Controls.Add(this.btnRegisterConnexion);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "FormRegister";
             this.Text = "Register";
             this.Load += new System.EventHandler(this.FormLogin_Load);
